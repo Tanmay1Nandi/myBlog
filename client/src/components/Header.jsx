455f3ed023ef20c748727c134703c426695f3ed1
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-04-01 18:33:08 +0530

    udpated Header Component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Button, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput} from "flowbite-react"
+import {Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput} from "flowbite-react"
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from "react-icons/ai"
 import {FaMoon} from "react-icons/fa"
+import {useSelector} from "react-redux"
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector(state => state.user);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold'>
@@ -25,9 +27,27 @@ export default function Header() {
         </Button>
         <div className='flex gap-2 md:order-2'>
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill><FaMoon /></Button>
+            {currentUser ? (
+                <Dropdown arrowIcon={false} inline crossOrigin="anonymous" 
+                label = {
+                    <Avatar alt="user" img={currentUser.profilePicture} rounded />
+                }
+                >
+                    <DropdownHeader>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                    </DropdownHeader>
+                    <Link to="/dashboard?tab=profile">
+                    <DropdownItem>Profile</DropdownItem></Link>
+                    <DropdownDivider/>
+                    <DropdownItem>Sign out</DropdownItem>
+                </Dropdown>
+            )
+              : 
             <Link to='/sign-in'>
-              <Button className='bg-gradient-to-r from-purple-600 to-blue-600'>Sign In</Button>
-            </Link>
+            <Button className='bg-gradient-to-r from-purple-600 to-blue-600'>Sign In</Button>
+          </Link>}
+            
             <NavbarToggle />
         </div>
         <NavbarCollapse>
