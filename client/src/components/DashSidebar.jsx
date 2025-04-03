5796f44b7b82b8699013873ca8448c5dee68de79
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-04-03 08:26:45 +0530

    created basic Dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,34 @@
+import {Sidebar, SidebarItem, SidebarItemGroup, SidebarItems} from "flowbite-react"
+import { HiArrowSmRight, HiUser } from "react-icons/hi"
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from "react-router-dom"
+
+export default function DashSidebar() {
+    const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabForUrl = urlParams.get("tab");
+    if(tabForUrl){
+      setTab(tabForUrl);
+    }
+  },[location.search])
+  return (
+    <div>
+        <Sidebar className="w-full md:w-56">
+            <SidebarItems>
+                <SidebarItemGroup className="md:min-h-screen flex flex-col gap-0.5">
+                    <Link to="/dashboard?tab=profile">
+                        <SidebarItem active={tab === "profile"} icon={HiUser} label={"User"} labelColor='dark'>
+                            Profile
+                        </SidebarItem>
+                    </Link>
+                    <SidebarItem icon={HiArrowSmRight}  labelColor='dark'>
+                        Sign out
+                    </SidebarItem>
+                </SidebarItemGroup>
+            </SidebarItems>
+        </Sidebar>
+    </div>
+  )
+}
