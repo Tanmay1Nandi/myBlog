d8e732fc080c87df4fea58096d2c537916007594
Tanmay1Nandi <118953751+Tanmay1Nandi@users.noreply.github.com>
2025-04-12 06:35:33 +0530

    added .jsx to import file name

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from "react-router-dom";
-import CallToAction from "../components/CallToAction";
+import CallToAction from "../components/CallToAction.jsx";
 import PostCard from '../components/PostCard';
 
 export default function Home() {
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CallToAction from '../components/CallToAction'
+import CallToAction from '../components/CallToAction.jsx'
 
 export default function Projects() {
   return (
